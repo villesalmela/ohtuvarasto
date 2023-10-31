88d2c73ac6ccf9214502b80daceabbbddcb05f63
Ville Salmela <villesalmela@users.noreply.github.com>
2023-10-31 22:36:06 +0200

    add missing unit tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -2,6 +2,23 @@ import unittest
 from varasto import Varasto
 
 
+class TestInvalidVarasto(unittest.TestCase):
+    def setUp(self) -> None:
+        self.varasto = Varasto(tilavuus=-10, alku_saldo=-11)
+
+    def test_tilavuus_nollattu(self):
+        self.assertEqual(self.varasto.tilavuus, 0.0)
+
+    def test_saldo_nollattu(self):
+        self.assertEqual(self.varasto.saldo, 0.0)
+
+class TestInvalidVarasto2(unittest.TestCase):
+    def setUp(self) -> None:
+        self.varasto = Varasto(tilavuus=10, alku_saldo=11)
+
+    def test_saldo_oikein(self):
+        self.assertEqual(self.varasto.saldo, 10.0)
+
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
@@ -38,3 +55,23 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_tyhja_lisays(self):
+        self.varasto.lisaa_varastoon(maara=-10)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_liian_paljon_lisays(self):
+        self.varasto.lisaa_varastoon(maara=100)
+        self.assertEqual(self.varasto.saldo, 10)
+
+    def test_ota_liikaa(self):
+        self.varasto.lisaa_varastoon(maara=10)
+        self.assertEqual(self.varasto.ota_varastosta(100), 10)
+
+    def test_ota_neg(self):
+        self.varasto.lisaa_varastoon(maara=10)
+        self.assertEqual(self.varasto.ota_varastosta(-10), 0.0)
+
+    def test_print(self):
+        self.varasto.lisaa_varastoon(7)
+        self.assertEqual(str(self.varasto), "saldo = 7, vielä tilaa 3")
