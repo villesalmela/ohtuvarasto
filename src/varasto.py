8f988db3c8e4aa5bac1cb9ef3bb679775368d54a
Ville Salmela <villesalmela@users.noreply.github.com>
2023-11-08 16:18:58 +0200

    break linting

--- src/varasto.py
+++ src/varasto.py
@@ -18,6 +18,7 @@ class Varasto:
 
     # huom: ominaisuus voidaan myös laskea.
     # Ei tarvita erillistä kenttää viela_tilaa tms.
+    # ..........................................................................................................................................................
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
